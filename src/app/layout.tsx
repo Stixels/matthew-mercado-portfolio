0fdcce997b6bcdcf9a97c4fd9c351e51ce79a785
Matthew Mercado <89710831+Stixels@users.noreply.github.com>
2023-07-28 14:11:05 -0400

    Resume Update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { Analytics } from "@vercel/analytics/react";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
